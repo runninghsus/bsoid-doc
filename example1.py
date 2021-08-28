a829647d19bfadb22a48acbc7c24c2a940b1292d
DESKTOP-S63O9Q6\alexa <alexander.hsu.han@gmail.com>
2021-08-28 14:22:24 -0400

    adding functions to document

---
+++ example1.py
@@ -0,0 +1,11 @@
+def func1(arg1, arg2):
+    """
+    This function takes two arguments, sets the first equals the second, then returns the new first argument
+
+    :param arg1: Some value
+    :param arg2: Another value
+
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
